13c58f6a277f706ee5819ad2da71c11f9383188c
surung9898 <surung9898@naver.com>
2018-11-30 12:46:37 +0900

    go for the top

---
+++ test11.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+	int a, b;
+
+	printf("¼ýÀÚ 2°³");
+	scanf("%d %d", &a, &b);
+
+	printf("%d", a + b);
+
+	system("pause");
+}
